9e0bfba45e0c9932a53ff62a468fcbcd509e057f
François KERVELLA <kervella.francois@gmail.com>
2026-04-24 12:06:51 +0200

    Modifications pour fonctionnement cors et mise à jour des packages

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const apiRouter = require('./routes/api');
@@ -11,7 +11,20 @@ const { initialize } = require('./db');
 
 const app = express();
 
+// Dans vos routes ou comme middleware global
+/*app.use((req, res, next) => {
+    const delay = 2000; // 2 secondes en millisecondes
+    setTimeout(() => {
+        next();
+    }, delay);
+});*/
+
 app.use(logger('dev'));
+app.use(cors({
+    origin:"http://localhost:8000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
